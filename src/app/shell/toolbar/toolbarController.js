3d9b427d88537914ab6e348cb2e0727b5c704d89
Klaas Kooistra <klaas@mywheels.nl>
2016-04-25 12:09:18 +0200

    New prototype homepage

--- src/app/shell/toolbar/toolbarController.js
+++ src/app/shell/toolbar/toolbarController.js
@@ -2,5 +2,16 @@
 
 angular.module('owm.shell')
 
-.controller('ToolbarController', function () {
-});
+.controller('ToolbarController', function ($state, $scope, resourceQueryService) {
+    $scope.doSearch = function (placeDetails) {
+      if (placeDetails) {
+        resourceQueryService.setText($scope.search.text);
+        resourceQueryService.setLocation({
+          latitude : placeDetails.geometry.location.lat(),
+          longitude: placeDetails.geometry.location.lng()
+        });
+      }
+      $state.go('owm.resource.search.list', resourceQueryService.createStateParams());
+    };
+
+  });
