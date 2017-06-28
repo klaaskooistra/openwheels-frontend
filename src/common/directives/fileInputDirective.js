75bb96728a9bf3175a8934078e1611bcce2c6818
Freek <freek.boutkan@gmail.com>
2017-06-28 13:37:29 +0200

    Half way done

--- src/common/directives/fileInputDirective.js
+++ src/common/directives/fileInputDirective.js
@@ -29,27 +29,11 @@ angular.module('fileInputDirective', [])
           console.log(answer);
         });
       };
-
-      if (angular.isFunction(scope.onChange)) {
-        elm.on('change', onChange);
-
-        scope.$on('$destroy', function () {
-          elm.off('change', onChange);
-        });
-      }
-
-      function onChange (e) {
-        scope.$apply(function () {
-          scope.onChange(e.target.files[0]);
-          if(scope.reset && scope.reset === 'noreset') {
-            return;
-          }
-          elm.val('');
-        });
-      }
     }
+
   };
 })
+
 .directive('fileInputChange', function () {
 
   return {
@@ -58,13 +42,23 @@ angular.module('fileInputDirective', [])
     replace: true,
     scope: {
       onChange: '=',
+      fileProps: '=',
       reset: '@',
     },
     link: function (scope, elm, attrs) {
 
       scope.id = 'file123';
-      console.log(elm);
-      console.log(elm.firstChild);
+
+      function clearFileProps() {
+        scope.uploaded = {
+          sizeOk: undefined,
+          height: undefined,
+          width: undefined,
+          isImage: undefined,
+          hasUploaded: false,
+        };
+      }
+      clearFileProps();
 
       if (angular.isFunction(scope.onChange)) {
         elm.on('change', onChange);
@@ -76,25 +70,78 @@ angular.module('fileInputDirective', [])
 
 
       function onChange (e) {
-        console.log(e);
-        scope.$apply(function () {
-          scope.onChange(e.target.files[0]);
-          if(scope.reset && scope.reset === 'noreset') {
-            return;
-          }
-          elm.val('');
-        });
+        scope.uploaded.hasUploaded = true;
+
+				window.URL = window.URL || window.webkitURL;
+        var img = new Image();
+        img.src = window.URL.createObjectURL( e.target.files[0] );
+
+        img.onload = function() {
+          var width = img.naturalWidth;
+          var height = img.naturalHeight;
+          window.URL.revokeObjectURL( img.src );
+
+          scope.uploaded.isImage = true;
+          scope.uploaded.width = width;
+          scope.uploaded.height = height;
+          scope.uploaded.sizeOk = width >= 800 && height >= 600;
+          updateScope();
+        };
+
+        img.onerror = function() {
+          scope.uploaded.isImage = false;
+          updateScope();
+        };
+
+        function updateScope() {
+          scope.$apply(function () {
+            if(scope.uploaded.isImage && scope.uploaded.sizeOk) {
+              scope.onChange(e.target.files[0]);
+            }
+            scope.fileProps = scope.uploaded;
+
+            if(scope.reset && scope.reset === 'noreset') {
+              return;
+            }
+            elm.val('');
+          });
+        }
+
       }
     }
   };
 })
+
 .controller('fileUploaderController', function($mdDialog, $scope) {
   $scope.image = '';
   $scope.inputImage = '';
 
+  $scope.aspectratio = 1.51;
+  $scope.resultImageSize = {w: 1000, h: 660};
+  $scope.rotated = false;
+
   $scope.newImage = function($event) {
+    var src = URL.createObjectURL($event);
     $scope.inputImage = $event;
-		console.log('set inputImage');
+  };
+
+  $scope.rotate = function(rotated) {
+
+    if(rotated !== undefined) {
+      $scope.rotated = rotated;
+    } else {
+      $scope.rotated = !$scope.rotated;
+    }
+    var aspectratio = 1.5151;
+
+    if($scope.rotated) {
+      $scope.aspectratio = 1 / aspectratio;
+      $scope.resultImageSize = {w: 660, h: 1000};
+    } else {
+      $scope.aspectratio = aspectratio;
+      $scope.resultImageSize = {w: 1000, h: 660};
+    }
+
   };
 
   $scope.clear = function() {
@@ -102,6 +149,8 @@ angular.module('fileInputDirective', [])
     $scope.inputImage = '';
     $scope.crapject = {};
     $scope.showEditor = false;
+    $scope.fileProps = {};
+    $scope.rotate(false);
   };
 
   $scope.return = function(returnValue) {
@@ -111,5 +160,43 @@ angular.module('fileInputDirective', [])
   $scope.cancel = function(reason) {
     $mdDialog.cancel(reason);
   };
+
+  $scope.doneEditing = function() {
+    $scope.showEditor = false;
+    if($scope.rotated) {
+      $scope.image = rotateBase64Image90deg($scope.image);
+    }
+  };
+
+  $scope.rotate180 = function() {
+  };
+
+  function rotateBase64Image90deg(base64Image, isClockwise) {
+    // create an off-screen canvas
+    var offScreenCanvas = document.createElement('canvas');
+    var offScreenCanvasCtx = offScreenCanvas.getContext('2d');
+
+    // cteate Image
+    var img = new Image();
+    img.src = base64Image;
+
+    // set its dimension to rotated size
+    offScreenCanvas.height = img.width;
+    offScreenCanvas.width = img.height;
+
+    // rotate and draw source image into the off-screen canvas:
+    if (isClockwise) {
+      offScreenCanvasCtx.rotate(90 * Math.PI / 180);
+      offScreenCanvasCtx.translate(0, -offScreenCanvas.width);
+    } else {
+      offScreenCanvasCtx.rotate(-90 * Math.PI / 180);
+      offScreenCanvasCtx.translate(-offScreenCanvas.height, 0);
+    }
+    offScreenCanvasCtx.drawImage(img, 0, 0);
+
+    // encode image to data-uri with base64
+    return offScreenCanvas.toDataURL('image/png', 100);
+  }
+
 })
 ;
