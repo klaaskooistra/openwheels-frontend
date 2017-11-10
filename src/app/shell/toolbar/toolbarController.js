32e5aab960b6789d1bc9c53978370965ee4eb93c
Aron <aron@mywheels.nl>
2017-11-10 14:16:35 +0100

    Typo's

--- src/app/shell/toolbar/toolbarController.js
+++ src/app/shell/toolbar/toolbarController.js
@@ -6,11 +6,8 @@ angular.module('owm.shell')
 
 	//if visitor is on the signup page, don't show the buttons in the toolbar because of distraction reasons
 	$scope.onSignUpPage = $rootScope.$state.current.name === 'owm.auth.signup' ? true : false;
-	console.log($scope.onSignUpPage);
 	$rootScope.$on('$stateChangeSuccess', function (event, toState) {
-		console.log(toState.name);
 		$scope.onSignUpPage = toState.name === 'owm.auth.signup' ? true : false;
-		console.log($scope.onSignUpPage);
 	});
 
 });
