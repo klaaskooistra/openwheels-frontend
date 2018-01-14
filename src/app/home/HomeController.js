2adc371d0e3a6f0c03d38e8b2048a01e2de78ae2
Aronv90 <avaas@hotmail.com>
2018-01-14 14:34:16 +0100

    Get location of visitor

--- src/app/home/HomeController.js
+++ src/app/home/HomeController.js
@@ -3,7 +3,7 @@
 angular.module('owm.home', ['owm.resource', 'slick'])
 
 //Module in app/pages/pagesModule.js
-.controller('HomeController', function ($scope, $translate, $location, resourceQueryService, $state, resourceService, $localStorage, $http) {
+.controller('HomeController', function ($scope, $translate, $location, resourceQueryService, $window, $state, resourceService, $localStorage, $http) {
 
   $scope.$watch(function () {
     return $translate.use();
@@ -31,20 +31,49 @@ angular.module('owm.home', ['owm.resource', 'slick'])
     });
   }
 
-  if ($scope.features.featuredSlider) {
-    resourceService.all({
-        'onlyFeatured': 'true'
-      })
-      .then(function (res) {
-        $scope.resources_slider = res;
-      });
-    $scope.gotoProfile = function (resource) {
-      $state.go('owm.resource.show', {
-        city: resource.city,
-        resourceId: resource.id
-      });
-    };
+  //get resources for slider if page is loaded
+  angular.element($window.document).ready(function () {
+    if ($scope.features.featuredSlider) {
+      if($window.navigator.geolocation) {
+        $window.navigator.geolocation.getCurrentPosition(getNearbyResources, getFeaturedResources);
+      } else {
+        getFeaturedResources();
+      }
+    }
+  });
+
+  //get resources nearby geolocation
+  function getNearbyResources(position) {
+    resourceService.searchV3({
+      location: {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      radius: 15000,
+      maxresults: 15,
+      sort: 'relevance'
+    })
+    .then(function (res) {
+      $scope.resources_slider = res.results;
+    });
   }
+
+  //get featured resources if no geolocation is available
+  function getFeaturedResources() {
+    resourceService.all({
+      'onlyFeatured': 'true'
+    })
+    .then(function (res) {
+      $scope.resources_slider = res;
+    });
+  }
+
+  $scope.gotoProfile = function (resource) {
+    $state.go('owm.resource.show', {
+      city: resource.city,
+      resourceId: resource.id
+    });
+  };
 
   $scope.search = {
     text: ''
