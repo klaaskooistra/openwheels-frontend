464e32402b586ff84d7f31b3eaaf1ba4bcd5246e
Aron Vaas <aron@mywheels.nl>
2018-02-06 12:36:39 +0100

    Fix signup flow

--- src/common/directives/signupFormDirective/signupFormDirective.js
+++ src/common/directives/signupFormDirective/signupFormDirective.js
@@ -117,76 +117,86 @@ angular.module('signupFormDirective', [])
           terms = $scope.auth.terms,
           preference = user.preference;
 
-        var captcha = user.captcha;
+        // var captcha = user.captcha;
 
-        if (email && password && user) {
-          if (preference) {
-            if (terms === true) {
-              authService.oauthSubscribe({
-                  email: email.trim().toLowerCase(),
-                  password: password,
-                  other: user,
-                  captcha: captcha
-                }).then(function (res) {
-                  Analytics.trackEvent('person', 'created', res.id, undefined, true);
-                  if ($scope.url === 'owm.person.details({pageNumber: \'1\'})') {
-                    var booking = $scope.booking;
-                    var resource = $scope.resource;
-                    $mdDialog.cancel();
-                    $state.go('owm.person.details', { // should fill in the details
-                      pageNumber: '1',
-                      city: resource.city ? resource.city : 'utrecht',
-                      resourceId: resource.id,
-                      startDate: booking.beginRequested,
-                      endDate: booking.endRequested,
-                      discountCode: booking.discountCode,
-                      remarkRequester: booking.remarkRequester,
-                      riskReduction: booking.riskReduction
-                    });
-                  } else if ($scope.url === 'owm.resource.create.carInfo') {
-                    var licencePlate = $scope.licencePlate;
-                    var calculateYourPrice = $scope.calculateYourPrice;
-                    var brand = $scope.brand;
-                    var model = $scope.model;
-                    var seats = $scope.seats;
-                    var color = $scope.color;
-                    var fuel = $scope.fuel;
-                    var type = $scope.type;
-                    var year = $scope.year;
-                    $mdDialog.cancel();
+        if (user.firstName) {
+          if (email) {
+            if (password) {
+              if (preference) {
+                if (terms === true) {
+                  authService.oauthSubscribe({
+                      email: email.trim().toLowerCase(),
+                      password: password,
+                      other: user
+                      // captcha: captcha
+                    }).then(function (res) {
+                      Analytics.trackEvent('person', 'created', res.id, undefined, true);
+                      if ($scope.url === 'owm.person.details({pageNumber: \'1\'})') {
+                        var booking = $scope.booking;
+                        var resource = $scope.resource;
+                        $mdDialog.cancel();
+                        $state.go('owm.person.details', { // should fill in the details
+                          pageNumber: '1',
+                          city: resource.city ? resource.city : 'utrecht',
+                          resourceId: resource.id,
+                          startDate: booking.beginRequested,
+                          endDate: booking.endRequested,
+                          discountCode: booking.discountCode,
+                          remarkRequester: booking.remarkRequester,
+                          riskReduction: booking.riskReduction
+                        });
+                      } else if ($scope.url === 'owm.resource.create.carInfo') {
+                        var licencePlate = $scope.licencePlate;
+                        var calculateYourPrice = $scope.calculateYourPrice;
+                        var brand = $scope.brand;
+                        var model = $scope.model;
+                        var seats = $scope.seats;
+                        var color = $scope.color;
+                        var fuel = $scope.fuel;
+                        var type = $scope.type;
+                        var year = $scope.year;
+                        $mdDialog.cancel();
 
-                    $state.go('owm.resource.create.carInfo', { // should fill in the details
-                      licencePlate: licencePlate.content,
-                      dayPrice: calculateYourPrice.dayPrice,
-                      numberOfDays: calculateYourPrice.numberOfDays,
-                      brand: brand,
-                      model: model,
-                      seats: seats,
-                      color: color,
-                      fuel: fuel,
-                      type: type,
-                      year: year
+                        $state.go('owm.resource.create.carInfo', { // should fill in the details
+                          licencePlate: licencePlate.content,
+                          dayPrice: calculateYourPrice.dayPrice,
+                          numberOfDays: calculateYourPrice.numberOfDays,
+                          brand: brand,
+                          model: model,
+                          seats: seats,
+                          color: color,
+                          fuel: fuel,
+                          type: type,
+                          year: year
+                        });
+                      } else {
+                        $state.go($scope.url);
+                      }
+                    })
+                    .catch(function (err) {
+                      alertService.add(err.level, err.message, 4000);
+                    })
+                    .finally(function () {
+                      alertService.loaded();
                     });
-                  } else {
-                    $state.go($scope.url);
-                  }
-                })
-                .catch(function (err) {
-                  alertService.add(err.level, err.message, 4000);
-                })
-                .finally(function () {
+                } else {
+                  alertService.add('danger', $translate.instant('SIGNUP_AGREE_TO_TERMS_ALERT'), 4000);
                   alertService.loaded();
-                });
+                }
+              } else {
+                alertService.add('danger', $translate.instant('SIGNUP_RENTER_OWNER_CHOICE_ALERT'), 4000);
+                alertService.loaded();
+              }
             } else {
-              alertService.add('danger', $translate.instant('SIGNUP_AGREE_TO_TERMS_ALERT'), 4000);
+              alertService.add('danger', $translate.instant('SIGNUP_FILL_IN_PASSWORD_ALERT'), 4000);
               alertService.loaded();
             }
           } else {
-            alertService.add('danger', $translate.instant('SIGNUP_RENTER_OWNER_CHOICE_ALERT'), 4000);
+            alertService.add('danger', $translate.instant('SIGNUP_FILL_IN_MAIL_ALERT'), 4000);
             alertService.loaded();
           }
         } else {
-          alertService.add('danger', $translate.instant('SIGNUP_FILL_IN_FIELDS_ALERT'), 4000);
+          alertService.add('danger', $translate.instant('SIGNUP_FILL_IN_FIRSTNAME_ALERT'), 4000);
           alertService.loaded();
         }
       };
